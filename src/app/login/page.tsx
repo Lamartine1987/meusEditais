7ab2fae553569a172c63b0b43908d2a653cc8054
Lamartine Cezar <lamartinecezar3@gmail.com>
2025-08-21 19:03:02 +0000

    npm run build
    > nextn@0.1.0 build
    > next build
    
      ▲ Next.js 14.2.4
      -

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,4 @@
+
 "use client";
 
 import { useState } from 'react';
@@ -14,7 +15,7 @@ import { useToast } from '@/hooks/use-toast';
 
 export default function LoginPage() {
   const router = useRouter();
-  const { login, loading: authLoading } from 'useAuth';
+  const { login, loading: authLoading } = useAuth();
   const { toast } = useToast();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
